3a36c88c4d676d2dd48a365ac8e53dcdbaa5193f
rajwinder kaur <rajwinderbhullar126@gmail.com>
2021-01-20 11:17:55 +1300

    change the coding

---
+++ dbo.Signup.data.sql
@@ -0,0 +1,9 @@
+﻿SET IDENTITY_INSERT [dbo].[Signup] ON
+INSERT INTO [dbo].[Signup] ([Id], [Signup_Name], [Signup_Email], [Signup_Subject], [Signup_Massage]) VALUES (1, N'Rajwinder Kaur', N'rajwinderbhullar126@gmail.com', N'FOR OPEN THE BROWSER', N'Thank you')
+INSERT INTO [dbo].[Signup] ([Id], [Signup_Name], [Signup_Email], [Signup_Subject], [Signup_Massage]) VALUES (2, N'Ritika Sharma', N'ritikasharma@gmail.com', N'FOR OPEN THE BROWSER', N'Thank you')
+INSERT INTO [dbo].[Signup] ([Id], [Signup_Name], [Signup_Email], [Signup_Subject], [Signup_Massage]) VALUES (3, N'simran', N'simran123@gmail.com', N'FOR OPEN THE BROWSER', N'Thank you')
+INSERT INTO [dbo].[Signup] ([Id], [Signup_Name], [Signup_Email], [Signup_Subject], [Signup_Massage]) VALUES (4, N'Sukhjit kaur', N'sukhjit123@gmail.com', N'FOR OPEN THE BROWSER', N'Thank you')
+INSERT INTO [dbo].[Signup] ([Id], [Signup_Name], [Signup_Email], [Signup_Subject], [Signup_Massage]) VALUES (5, N'Gagandeep kaur', N'gagandeepkaur1234678@gmail.com', N'FOR OPEN THE BROWSER', N'Thank you')
+INSERT INTO [dbo].[Signup] ([Id], [Signup_Name], [Signup_Email], [Signup_Subject], [Signup_Massage]) VALUES (6, N'Gurbhej singh', N'gurbhejsekhon2@gmail.com', N'FOR OPEN THE BROWSER', N'Thank you')
+INSERT INTO [dbo].[Signup] ([Id], [Signup_Name], [Signup_Email], [Signup_Subject], [Signup_Massage]) VALUES (7, N'Rajdeep kaur', N'rajdeepkaur12345@gmail.com', N'FOR OPEN THE BROWSER', N'Thank you')
+SET IDENTITY_INSERT [dbo].[Signup] OFF
